5a025606b2d70c18dc907bf9821e95cf0bdd60b3
Warren Hood <nullbyte001@gmail.com>
2018-10-17 12:39:23 +0200

    Add files via upload

--- parse.cpp
+++ parse.cpp
@@ -47,35 +47,55 @@ class TreeNode{
 				this->ptrfunc = ptr;
 				this->type = Function;
 			};
-			
+
 };
-std::vector<TreeNode> lex(const std::string& cp,std::vector<std::string> splits,std::vector<std::string> ignores){
-	std::vector<std::string> tokens;
-	std::string current = "";
-	for(int i=0; i<cp.size(); i++){
+void operator+=(std::vector<char>& v,char e){
+	v.push_back(e);
+}
+std::vector<std::vector<char>> lex(char* cp,std::vector<char> splits,std::vector<char> ignores){
+	std::vector<std::vector<char>> tokens;
+	std::vector<char> current;
+	for(;*cp != '\0';cp++){
 		bool found_in_splits = false;
 		bool found_in_ignores = false;
-		for(auto sym:splits){
-			if(sym == cp[i]){found_in_splits = true; break;}
+		//Check if char is in splits vec
+		for(char sym:splits){
+			if(sym == static_cast<char>(*cp)){found_in_splits = true; break;}
 		}
+		//Check if char is in ignores vec
 		if(!found_in_splits) for(auto sym:ignores){
-			if(sym == cp[i]){found_in_ignores=true; break;}
+			if(sym == *cp){found_in_ignores=true; break;}
 		}
+		//If char was in ignores
 		if(found_in_ignores){
-			tokens.push_back(current);
-			current = "";
+			//Push current into tokens
+			if(current.size())tokens.push_back(current);
+			//Set current to empty new vec
+			current = *(new std::vector<char> {});
 		}
-		else current += cp[i];
+		//If char was in splits
 		if(found_in_splits){
-			tokens.push_back(current);
-			current = "";
+			//Push current into tokens
+			if(current.size())tokens.push_back(current);
+			//Push the char into tokens
+			tokens.push_back(std::vector<char>{*cp});
+			//Set current to empty new vec
+			current = *(new std::vector<char> {});
 		}
+		else if(!found_in_ignores) current.push_back(static_cast<char>(*cp));
 	}
+	if(current.size())tokens.push_back(current);
+	return tokens;
 };
-std::ostream& operator<<(std::ostream& os,std::vector<std::string> v){
+std::ostream& operator<<(std::ostream& os,std::vector<std::vector<char>> v){
 	os << "{";
-	if(v.size())os<<v[0];
-	for(int i=1; i<v.size(); i++)os << "," << v[i];
+	if(v.size())for(auto ch:v[0])os << "\"" << ch << "\"";
+	for(int i=1; i<v.size(); i++){
+		os << " , ";
+		os << "\"";
+		for(auto ch:v[i])os<<ch;
+		os << "\"";
+	}
 	os << "}";
 	return os;
 }
@@ -86,7 +106,7 @@ std::ostream& operator<<(std::ostream& os,std::vector<std::string> v){
 	define TEST to run it.
 */
 int main(int argc,char** argv){
-	std::string parse_string = "<int> x=1; <int> y=2; print.{x+y};";
-	std::cout << lex(parse_string);
+	char parse_string[] = "<int> x=1; <int> y=2; print.{x+y};";
+	std::cout << lex(parse_string,{'<','>','{','}',';','=','.','+','-'},{' ','\r','\n'});
 }
 #endif
